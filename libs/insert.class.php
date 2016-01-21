ed60488deca505d4e8dfc9d9cae305c202d609a7
Emiliano Maccaferri <emilianomaccaferri@gmail.com>
2016-01-22 00:37:56 +0100

    Update insert.class.php

--- libs/insert.class.php
+++ libs/insert.class.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
   class InsertPlayer{
@@ -30,38 +30,3 @@
 }
 
 ?>
-=======
-<?php
-
-//I will add more thing, that's because it's a class.
-
-  class InsertPlayer{
-
-    public function insert($player, $reason, $date, $ID, $realID){
-
-   if($ID == $realID){
-     $writePlayer = fopen("players/$player.json", 'w');
-
-     $infos[] = array(
-
-       "name" => $player,
-       "reason" => $reason,
-       "date" => $date
-
-     );
-
-     $message[] = array( "message"=> "Player $player has been added");
-     echo json_encode($message);
-     fwrite($writePlayer, json_encode($infos));
-     fclose($writePlayer);
-
-   }else{
-   $message[] = array( "error" => "Wrong ID");
-     die(json_encode($message));
-   }
-
-  }
-}
-
-?>
->>>>>>> 544b710d4deb82e329430be54366bbcbe1152bde
