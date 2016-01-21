3a0a4cca621b15e3428e8ee9ed652ac6ad7f427c
Emiliano Maccaferri <emilianomaccaferri@gmail.com>
2016-01-22 00:39:06 +0100

    Update readall.class.php

--- libs/readall.class.php
+++ libs/readall.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
   class ReadAll{
@@ -49,57 +48,3 @@
   }
 
 ?>
-=======
-<?php
-
-//I will add more thing, that's because it's a class.
-
-  class ReadAll{
-
-    public function read($ID, $realID){
-
-      if($ID == $realID){
-
-        function pregJson($data){
-
-          return preg_replace("/%20/", " ", json_encode($data));
-
-        }
-
-        $files = glob("players/*");
-        $nFiles = count($files);
-
-        $jsonarray[] = array();
-
-        foreach($files as $file){
-
-          $data = json_decode(file_get_contents($file), true);
-          $name = $data[0]["name"];
-          $reason = $data[0]["reason"];
-          $date = $data[0]["date"];
-
-          $jsonArray[] = array(
-
-            "name" => $name,
-            "reason" => $reason,
-            "date" => $date
-
-          );
-
-
-        }
-
-        echo pregJson($jsonArray);
-
-
-    }else{
-    $message[] = array( "error" => "Wrong ID");
-      die(json_encode($message));
-    }
-
-    }
-
-  }
-
-?>
->>>>>>> 544b710d4deb82e329430be54366bbcbe1152bde
