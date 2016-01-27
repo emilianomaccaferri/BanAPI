d54aba9078c0306e97076181de91e04742245d64
Macca_ <emilianomaccaferri@gmail.com>
2016-01-27 03:41:02 -0500

    player-actions.class added

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # BanAPI
+<<<<<<< HEAD
+This Rest API can help you to manage bans	
+=======
 This Rest API helps you to manage your Minecraft server's bans!
 
 #How does it work?
-You don't want to have a database on your server? BanAPI is the solution!
 By simply making a request to your website, this API can register, remove and read banned players.
 It creates a file for each player you insert and retrives infos from there.
 You will be provided of a secure ID at the first run of the API (or if the config.json file is missing).
 
-**REMEMBER TO MAKE THE /data FOLDER NON-ACCESSIBLE FROM WEB**
+**REMEMBER TO MAKE THE /data FOLDER NON-ACCESSIBLE**
 
 #Create a ban
 If you want to create a ban, the "syntax" is simple: `http://yourdomain.com/insert/player/date/reason/yourID`.
@@ -17,6 +19,4 @@ If you want to remove a ban, simply access to: `http://yourdomain.com/remove/pla
 
 #Read all the bans
 To read all the bans, simply access to: `http://yourdomain.com/readall/yourID`.
-
-#Read a single ban
-To read a single ban, simply access to: `https://yourdomain.com/read/name/yourID`
+>>>>>>> 544b710d4deb82e329430be54366bbcbe1152bde
--- index.php
+++ index.php
@@ -1,111 +1,120 @@
 <?php
-set_error_handler('exceptions_error_handler');
-header('Content-Type: application/json');
-ini_set('display_errors', 1);
-require "libs/insert.class.php";
-require "libs/readall.class.php";
-require "libs/remove.class.php";
-require "libs/read.class.php";
+set_error_handler( 'exceptions_error_handler' );
+header( 'Content-Type: application/json' );
+ini_set( 'display_errors', 1 );
 
-$url = $_SERVER['REQUEST_URI'];
-$args = explode("/", $url);
-$argsLength = sizeof($args);
+///// LIBS /////
+require 'libs/player-actions.class.php';
+////////////////
 
-///// CLASSES /////
-$insertPlayer = new InsertPlayer();
-$removePlayer = new RemovePlayer();
-$readAll = new ReadAll();
-$readSingle = new Read();
-//////////////////
+$url        = $_SERVER[ 'REQUEST_URI' ];
+$args       = explode( "/", $url );
+$argsLength = sizeof( $args );
 
-function exceptions_error_handler($severity, $message, $filename, $lineno) {
-  if (error_reporting() == 0) {
-    return;
-  }
-  if (error_reporting() & $severity) {
-    throw new ErrorException($message, 0, $severity, $filename, $lineno);
-  }
+///// LOAD METHODS /////
+$actions = new PlayerActions();
+///////////////////////
+
+function exceptions_error_handler( $severity, $message, $filename, $lineno )
+{
+    if ( error_reporting() == 0 )
+    {
+        return;
+    }
+    if ( error_reporting() & $severity )
+    {
+        throw new ErrorException( $message, 0, $severity, $filename, $lineno );
+    }
 }
 
-try{
-if(!file_exists("data/config.json")){
+try
+{
+    if ( !file_exists( "data/config.json" ) )
+    {
 
-$key = md5(microtime().rand());
+        $key = md5( microtime() . rand() );
 
-  $json[] = array(
+        $json[ ] = array(
 
-    "id" => $key);
+             "id" => $key
+        );
 
-  $fp = fopen("data/config.json", 'w');
-  fwrite($fp, json_encode($json));
-  fclose($fp);
+        $fp = fopen( "data/config.json", 'w' );
+        fwrite( $fp, json_encode( $json ) );
+        fclose( $fp );
 
-  die("ID registered in your config.json file. Refresh the page to get the API work.");
+        die( "ID registered in your config.json file. Refresh the page to get the API work." );
+
+    }
+    $getJson = file_get_contents( "data/config.json" );
+    $data    = json_decode( $getJson, true );
+
+
+    $realID = $data[ 0 ][ "id" ];
+    $action = $args[ 1 ];
+
+    switch ( $action )
+    {
+
+        case 'insert':
+
+            $player = $args[ 2 ];
+            $date   = $args[ 3 ];
+            $reason = $args[ 4 ];
+            $ID     = $args[ 5 ];
+
+            $actions->insert( $player, $reason, $date, $ID, $realID );
+
+            break;
+
+        case 'remove':
+
+            $player = $args[ 2 ];
+            $ID     = $args[ 3 ];
+
+            $actions->remove( $player, $ID, $realID );
+
+            break;
+
+        case 'readall':
+
+            $ID = $args[ 2 ];
+
+            $actions->read( $ID, $realID );
+
+            break;
+
+        case 'read':
+
+            $player = $args[ 2 ];
+            $ID     = $args[ 3 ];
+
+            $actions->readPlayer( $player, $ID, $realID );
+
+            break;
+
+        default:
+
+            $message[ ] = array(
+                 "error" => "Invalid request: no action specified"
+            );
+            die( json_encode( $message ) );
+
+            break;
+
+    }
 
 }
-$getJson = file_get_contents("data/config.json");
-$data = json_decode($getJson, true);
+catch ( Exception $e )
+{
 
+    $errors[ ] = array(
 
-$realID = $data[0]["id"];
-$action = $args[1];
+         "error" => $e->getMessage()
 
- switch($action){
+    );
 
-   case 'insert':
-
-   $player = $args[2];
-   $date = $args[3];
-   $reason = $args[4];
-   $ID = $args[5];
-
-   $insertPlayer->insert($player, $reason, $date, $ID, $realID);
-
-  break;
-
-  case 'remove':
-
-  $player = $args[2];
-  $ID = $args[3];
-
-  $removePlayer->remove($player, $ID, $realID);
-
-    break;
-
-  case 'readall':
-
-  $ID = $args[2];
-
-  $readAll->read($ID, $realID);
-
-  break;
-
-  case 'read':
-
-  $player = $args[2];
-  $ID = $args[3];
-
-  $readSingle->readPlayer($player, $ID, $realID);
-
-  break;
-
-  default:
-
-  $message[] = array( "error" => "Invalid request: no action specified");
-    die(json_encode($message));
-
-  break;
-
-  }
-}catch (Exception $e){
-
-  $errors[] = array(
-
-    "error" => $e->getMessage()
-
-  );
-
-  echo json_encode($errors, JSON_UNESCAPED_SLASHES);
+    echo json_encode( $errors, JSON_UNESCAPED_SLASHES );
 
 }
 ?>
