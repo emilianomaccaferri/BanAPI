47f76c19b5f6f74a948fe203f071f13fbf600c76
Macca_ <emilianomaccaferri@gmail.com>
2016-03-06 18:24:17 -0500

    Version 1.0

--- example.php
+++ example.php
@@ -1,21 +1,11 @@
-<html>
-<meta charset="utf-8">
 <?php
 ini_set('display_errors', 1);
 $url = "http://yourdomain.com/readall/id";
 $content = file_get_contents($url);
-
 $data = json_decode($content, true);
-
   if(array_key_exists("error", $data[0])){
-
     die($data[0]["error"]);
-
   }
-
   echo var_dump($content);
-
-   echo json_encode($data[0]["name"]);  
-
+   echo json_encode($data[0]["name"]);
 ?>
-</html>
--- libs/player-actions.class.php
+++ libs/player-actions.class.php
@@ -1,48 +1,33 @@
 <?php
-
 class PlayerActions
 {
 
+  public static function pregJson( $data ){
+
+      return preg_replace( "/%20/", " ", json_encode( $data ) );
+  }
+
     public function read( $ID, $realID )
     {
-
         if ( $ID == $realID )
         {
 
-            function pregJson( $data )
-            {
-
-                return preg_replace( "/%20/", " ", json_encode( $data ) );
-
-            }
-
             $files  = glob( "players/*" );
             $nFiles = count( $files );
-
             $jsonarray[ ] = array( );
-
             foreach ( $files as $file )
             {
-
                 $data   = json_decode( file_get_contents( $file ), true );
                 $name   = $data[ 0 ][ "name" ];
                 $reason = $data[ 0 ][ "reason" ];
                 $date   = $data[ 0 ][ "date" ];
-
                 $jsonArray[ ] = array(
-
                      "name" => $name,
                     "reason" => $reason,
                     "date" => $date
-
                 );
-
-
             }
-
-            echo pregJson( $jsonArray );
-
-
+            echo $this->pregJson( $jsonArray );
         }
         else
         {
@@ -51,31 +36,23 @@ class PlayerActions
             );
             die( json_encode( $message ) );
         }
-
     }
-
     public function insert( $player, $reason, $date, $ID, $realID )
     {
-
         if ( $ID == $realID )
         {
             $writePlayer = fopen( "players/$player.json", 'w' );
-
             $infos[ ] = array(
-
                  "name" => $player,
                 "reason" => $reason,
                 "date" => $date
-
             );
-
             $message[ ] = array(
                  "message" => "Player $player has been added"
             );
             echo json_encode( $message );
             fwrite( $writePlayer, json_encode( $infos ) );
             fclose( $writePlayer );
-
         }
         else
         {
@@ -84,32 +61,23 @@ class PlayerActions
             );
             die( json_encode( $message ) );
         }
-
     }
-
     public function remove( $player, $ID, $realID )
     {
-
         if ( $ID == $realID )
         {
-
             if ( !file_exists( "players/$player.json" ) )
             {
-
                 $message[ ] = array(
                      "error" => "Player $player not found"
                 );
                 die( json_encode( $message ) );
-
             }
-
             unlink( "players/$player.json" );
-
             $message[ ] = array(
                  "message" => "Player $player has been deleted"
             );
             echo json_encode( $message );
-
         }
         else
         {
@@ -119,47 +87,29 @@ class PlayerActions
             die( json_encode( $message ) );
         }
     }
-
     public function readPlayer( $name, $ID, $realID )
     {
-
         if ( $ID == $realID )
         {
-
-            function pregJson( $data )
-            {
-
-                return preg_replace( "/%20/", " ", json_encode( $data ) );
-
-            }
 
             $path     = "players/$name.json";
             $data     = json_decode( file_get_contents( $path ), true );
             $reason   = $data[ 0 ][ "reason" ];
             $date     = $data[ 0 ][ "date" ];
             $infos[ ] = array(
-
                  'name' => $name,
                 'reason' => $reason,
                 'date' => $date
-
             );
-
-            echo pregJson( $infos );
-
+            echo $this->pregJson( $infos );
         }
         else
         {
-
             $message[ ] = array(
                  'error' => 'ID not found'
             );
             echo json_encode( $message );
-
         }
-
     }
-
 }
-
 ?>
